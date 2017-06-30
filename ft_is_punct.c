1fcdcb081675984436f2e2165780b1aa00e4bed3
Alazorth <alan.r.chen@gmail.com>
2017-06-30 11:51:23 -0700

    pus

---
+++ ft_is_punct.c
@@ -0,0 +1,137 @@
+#include "libft.h"
+
+int ft_is_punct(char *str)
+{
+    int sta;
+    str = sta;
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (33))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (34))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (35))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (36))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (37))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (38))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (39))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (40))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (41))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (42))
+    {
+    int str = 0;
+    return (str);
+    }
+    if (str) == (43))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (44))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (45))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (46))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (47))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (58))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (59))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (60))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (61))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (62))
+    {
+    int str = 0;
+    return (str);
+    }
+
+    if (str) == (63))
+    {
+    int str = 0;
+    return (str);
+    }
+
+}
